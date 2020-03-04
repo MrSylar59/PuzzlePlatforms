fda8a4af556dad5e04f01575745802a29900a828
Thomas De Maen <mr-sylar59@world-of-sylar.fr>
2020-03-04 19:57:01 +0100

    Added Trigger to platform

--- Source/PuzzlePlatforms/PlatformTrigger.cpp
+++ Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -15,6 +15,9 @@ APlatformTrigger::APlatformTrigger()
 	TriggerVolume = CreateDefaultSubobject<UBoxComponent>(FName("Trigger Volume"));
 	if (!ensure(TriggerVolume != nullptr)) return; // On évite les problèmes de pointeurs null
 	RootComponent = TriggerVolume;
+	// On lie la fonction OnOverlapBegin() à l'événement OnComponentBeginOverlap
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called when the game starts or when spawned
@@ -31,3 +34,20 @@ void APlatformTrigger::Tick(float DeltaTime)
 
 }
 
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, 
+						AActor* OtherActor, 
+						UPrimitiveComponent* OtherComp, 
+						int32 OtherBodyIndex, 
+						bool bFromSweep, 
+						const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, 
+					AActor* OtherActor, 
+					UPrimitiveComponent* OtherComp, 
+					int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
+}
